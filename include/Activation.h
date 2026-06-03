3dbddca680894d3c018fd303644f576a5df84da5
2023JoshuaP <jphilco@unsa.edu.pe>
2026-06-02 22:51:29 -0500

    Activations functions to MLP

---
+++ include/Activation.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include "Matrix.h"
+#include <string>
+
+struct ActivationFunction {
+    virtual ~ActivationFunction() {}
+    virtual Matrix forward(const Matrix &z) const = 0;
+    virtual Matrix derivative(const Matrix &a) const = 0;
+    virtual string name() const = 0;
+};
+
+struct Sigmoid : public ActivationFunction {
+    Matrix forward(const Matrix &z) const override;
+    Matrix derivative(const Matrix &a) const override;
+    string name() const override {
+        return "sigmoid";
+    }
+};
+
+struct ReLU : public ActivationFunction {
+    Matrix forward(const Matrix &z) const override;
+    Matrix derivative(const Matrix &a) const override;
+    string name() const override {
+        return "ReLU";
+    }
+};
+
+struct Softmax {
+    static Matrix forward(const Matrix &z);
+};
